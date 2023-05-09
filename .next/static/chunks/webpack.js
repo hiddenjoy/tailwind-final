2d039d5cb5f120c2fd8ce88e28a62e79a967fa0b
hiddenjoy <hiddenjoy@snu.ac.kr>
2023-05-10 01:51:00 +0900

    add: naver authentication

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1718563ce0bc805b"; }
+/******/ 		__webpack_require__.h = function() { return "5407286109848fe1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
 
 export default NextAuth({
   providers: [
@@ -12,6 +13,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+    NaverProvider({
+      clientId: process.env.NAVER_ID,
+      clientSecret: process.env.NAVER_SECRET,
     }),
   ],
 
